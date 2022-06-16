8bbb786c6550a908c3ae572c8bfefb7662c79bee
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-16 22:56:43 +0900

    feat: seed_fuによる学部情報のインポート、及び大学情報CSVの修正

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,3 +1,4 @@
 class University < ApplicationRecord
   belongs_to :prefecture
+  has_many :faculties
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_030122) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_110632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_030122) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.string "name"
+    t.bigint "university_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["university_id"], name: "index_faculties_on_university_id"
   end
 
   create_table "prefectures", force: :cascade do |t|
@@ -44,5 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_030122) do
     t.index ["prefecture_id"], name: "index_universities_on_prefecture_id"
   end
 
+  add_foreign_key "faculties", "universities"
   add_foreign_key "universities", "prefectures"
 end
