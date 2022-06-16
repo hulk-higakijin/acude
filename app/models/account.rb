8e4b408b0f5ca9849e3c25fa0e354dffd58c0aeb
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-16 12:17:17 +0900

    refactor: 改行の追加、削除

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,4 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum status: { candidate: 0, professor: 1 } ## school_staff: 2, student:3を追加予定
-
 end
