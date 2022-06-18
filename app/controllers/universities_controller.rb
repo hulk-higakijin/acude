1124bdb4c6253fff95a46ebd093e70a122a25c08
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-19 00:24:37 +0900

    都道府県ごとに大学を表示

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,13 +1,18 @@
 class UniversitiesController < ApplicationController
+  before_action :set_prefecture, only: [:index]
   before_action :set_university, only: [:show]
 
   def index
-    @universities = University.active
+    @universities = @prefecture ? @prefecture.universities.active : University.active
   end
 
   def show; end
 
   private
+
+    def set_prefecture
+      @prefecture = Prefecture.find_by(id: params[:prefecture_id])
+    end
 
     def set_university
       @university = University.find(params[:id])
