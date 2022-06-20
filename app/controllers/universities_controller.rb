528fb54f03a24ce0c6ad6b6705b7d3e9bae26684
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-20 22:38:30 +0900

    perf: 検索にストロングパラメータを使用

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -3,7 +3,7 @@ class UniversitiesController < ApplicationController
   before_action :set_university, only: [:show]
 
   def index
-    @universities = @prefecture ? @prefecture.universities.result(params) : University.result(params)
+    @universities = @prefecture ? @prefecture.universities.result(search_params) : University.result(search_params)
   end
 
   def show; end
@@ -16,5 +16,9 @@ class UniversitiesController < ApplicationController
 
     def set_university
       @university = University.active.find(params[:id])
+    end
+
+    def search_params
+      params.permit(:name)
     end
 end
