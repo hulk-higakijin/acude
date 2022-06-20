372b95d7f1ff79252108400d32f8c271e93620c6
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-20 22:13:00 +0900

    perf: 負担軽減のためsetTimeoutをインスタントサーチに用いる

--- app/javascript/controllers/prefecture_controller.js
+++ app/javascript/controllers/prefecture_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   submit() {
-    this.element.requestSubmit()
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 100)
   }
 }
