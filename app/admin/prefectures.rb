35533c1b9b056bb5ea1f4fa34575b41e3be7ad60
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-23 01:16:05 +0900

    refactor: rubocop修正

--- app/admin/prefectures.rb
+++ app/admin/prefectures.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register Prefecture do
-  config.sort_order = "id_asc"
+  config.sort_order = 'id_asc'
   config.clear_action_items!
   actions :index, :show
 
   index do
-    column :id
-    column :name
-    column :created_at
+    %i[id name created_at].each do |i|
+      column i
+    end
   end
 end
--- app/admin/universities.rb
+++ app/admin/universities.rb
@@ -1,46 +1,26 @@
 ActiveAdmin.register University do
-  config.sort_order = "id_asc"
+  config.sort_order = 'id_asc'
   config.clear_action_items!
   actions :index, :show
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :introduction, :prefecture_id, :url, :note, :active
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :introduction, :prefecture_id, :url, :note, :active]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-
   index do
-    column :id
-    column :name
-    column :prefecture
-    column :active
-    column :updated_at
+    %i[id name prefecture active updated_at].each do |i|
+      column i
+    end
     actions
   end
 
   show do
     attributes_table do
-      row :id
-      row :prefecture
-      row :name
-      row :faculties
-      row :url do |e|
-        link_to e.url, e.url, target: "_blank"
+      %i[id prefecture name faculties note introduction].each do |i|
+        row i
       end
-      row :note
-      row :introduction
+      row :url do |e|
+        link_to e.url, e.url, target: '_blank'
+      end
     end
   end
-  
+
   controller do
     def scoped_collection
       super.includes :prefecture
