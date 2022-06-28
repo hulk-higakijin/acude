ac618f842a7dbb2826a5a18fb555bc664a777f74
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-29 00:10:10 +0900

    feat: navbarにアイコン&ドロップダウンを設定

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,5 +8,6 @@ window.Stimulus   = application
 
 export { application }
 
-import { Modal } from "tailwindcss-stimulus-components"
+import { Modal, Dropdown } from "tailwindcss-stimulus-components"
 application.register('modal', Modal)
+application.register('dropdown', Dropdown)
