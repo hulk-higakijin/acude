449d7bc6f6206eebaf987573698f6976c46d2420
hulk-higakijin <hm385.chejptks@gmail.com>
2022-06-30 00:26:52 +0900

    test: profiles#showのテストを作成（のちに教授のテストも書く予定)

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
+  describe "GET #show（GET '/profile'）" do
+    before do
+      @account = create(:account)
+      sign_in @account
+      @account.create_candidate(name: 'hogehoge', introduction: 'foobar!')
+    end
+    it 'ステータス200を返すこと' do
+      get detail_profile_path
+      expect(response).to have_http_status(200)
+    end
+
+    # 未ログイン時：アクセスできない。
+    # ログイン時：アクセスできる。表示されるのは自分の名前
+  end
+
   describe "GET #new" do
     context '未ログイン時' do
       it 'ログインページにリダイレクトされること' do
